40af17a19529d04dcb85ddfe2989b097009d2e59
Ansar <ansarbagmirza@cdnsol.com>
2017-02-25 16:34:57 +0530

    show results on page and commenting code

--- app/controllers/assignments_controller.rb
+++ app/controllers/assignments_controller.rb
@@ -7,7 +7,8 @@ class AssignmentsController < ApplicationController
     #   words.push word.chop
     # end
     @message = "Numbers must be 10 digits long" and return if params[:phone_number].length != 10
-    @message = "Wrong Numbers" if params[:phone_number].split('').select{|a|(a.to_i == 0 || a.to_i == 1)}.length > 0
+    @message = "Wrong Numbers" and return if params[:phone_number].split('').select{|a|(a.to_i == 0 || a.to_i == 1)}.length > 0
+    @words = NumberToWord.new.letter_combinations(params[:phone_number])
   end
 
 end
--- app/models/number_to_word.rb
+++ app/models/number_to_word.rb
@@ -1,28 +1,35 @@
-# a recursive method
+
 class NumberToWord
 
-
   def letter_combinations(digits)
+    #return if number not valid
     return [] if digits.blank? || digits.length != 10 || digits.split('').select{|a|(a.to_i == 0 || a.to_i == 1)}.length > 0
+    #number to letters mapping
     letters = {"2" => ["a", "b", "c"],"3" => ["d", "e", "f"],"4" => ["g", "h", "i"],"5" => ["j", "k", "l"],"6" => ["m", "n", "o"],"7" => ["p", "q", "r", "s"],"8" => ["t", "u", "v"],"9" => ["w", "x", "y", "z"]}
+
+    # Read dictionary file and hold all values in a array
     dictionary = []
     file_path = Rails.root.join("db","dictionary.txt")
     File.foreach( file_path ) do |word|
       dictionary.push word.chop.to_s.downcase
     end
+    # get all letters for numbers in form of array
     keys = digits.chars.map{|digit|letters[digit]}
+
     results = {}
-    total_number = keys.length - 1
+    total_number = keys.length - 1 # total numbers
+    #Loo through all letters and get matching records with dictionary
     for i in (2..total_number)
       first_array = keys[0..i]
       second_array = keys[i + 1..total_number]
       next if first_array.length < 3 || second_array.length < 3
-      first_combination = first_array.shift.product(*first_array).map(&:join)
+      first_combination = first_array.shift.product(*first_array).map(&:join) # Get product of arrays
       next if first_combination.blank?
       second_combination = second_array.shift.product(*second_array).map(&:join)
       next if second_combination.blank?
-      results[i] = [(first_combination & dictionary), (second_combination & dictionary)]
+      results[i] = [(first_combination & dictionary), (second_combination & dictionary)] # get common values from arrays
     end
+    #arrange words like we need as a output
     final_words = []
     results.each do |key, combinataions|
       next if combinataions.first.blank? || combinataions.last.blank?
@@ -30,56 +37,9 @@ class NumberToWord
         final_words << combo_words
       end
     end
+    # for all numbers
     final_words << (keys.shift.product(*keys).map(&:join) & dictionary).join(", ") # matche with all character
     final_words
   end
-
-  def product_of_array(keys, words=[])
-    temp_array = []
-    unless keys.blank?
-      keys.each do |array_of_letters|
-        if words.blank?
-          words = array_of_letters
-        else
-          temp_array = words.dup
-          words = words.product(array_of_letters).map(&:join)
-          words.flatten!
-          words << temp_array
-          words.flatten!
-        end
-      end
-      #keys.shift
-      #words, keys = product_of_array(keys, words)
-    end
-    return words.reject{|a|a.length < 3}
-  end
-
-
-    # @return a list of strings, [s1, s2]
-  # def letter_combinations(digits)
-  #   # define a dictionary of the character <-> number projection
-  #   pad = {'2':'abc', '3':'def', '4':'ghi', '5':'jkl', '6':'mno', '7':'pqrs', '8':'tuv', '9':'wxyz'}
-  #   # if the input is empty
-  #   if digits.blank? || digits.length == 0
-  #     return [""]
-  #   end
-
-  #   # for number string length equals n-1
-  #   d0 = digits[-1]
-  #   comb0 = self.letter_combinations(d0)
-
-  #   # combine the last digit with previous n-1 string
-  #   d1 = digits[-1]
-  #   string1 = pad[d1]
-  #   comb1 = []
-  #   #  consider every combinations
-  #   for s in string1
-  #     for c in comb0
-  #       cs = c+s
-  #       comb1.push(cs)
-  #     end
-  #   end
-  #   return comb1
-  # end
 
 end
