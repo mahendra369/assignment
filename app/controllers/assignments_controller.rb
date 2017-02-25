72a8878ec88aa8e4cc1b335ff3d95558550f98bc
Ansar <ansarbagmirza@cdnsol.com>
2017-02-25 12:06:00 +0530

    Correct validations

--- app/controllers/assignments_controller.rb
+++ app/controllers/assignments_controller.rb
@@ -6,9 +6,8 @@ class AssignmentsController < ApplicationController
     # File.foreach( file_path ) do |word|
     #   words.push word.chop
     # end
-    #debugger
-    @message = "Wrong Numbers" if params[:phone_number].split('').select{|a|(a.to_i == 0 || a.to_i == 1)}.length > 1
-    render :index
+    @message = "Numbers must be 10 digits long" and return if params[:phone_number].length != 10
+    @message = "Wrong Numbers" if params[:phone_number].split('').select{|a|(a.to_i == 0 || a.to_i == 1)}.length > 0
   end
 
 end
